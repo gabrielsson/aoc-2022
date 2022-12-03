d4583d79a2f27926116f9f7bea1af21a0c25cc96
Max Gabrielsson <max.gabrielsson@gmail.com>
2022-12-03 12:01:07 +0100

    Test result
    Signed-off-by: Max Gabrielsson <max.gabrielsson@gmail.com>

--- src/test/scala/dev/gabrielsson/Day3Suite.scala
+++ src/test/scala/dev/gabrielsson/Day3Suite.scala
@@ -13,12 +13,12 @@ class Day3Suite extends AnyFlatSpec with Matchers {
     day.part1(day.getTestInput) shouldBe 157
   }
   it should "part1" in {
-    day.part1(day.getInput) shouldBe -1
+    day.part1(day.getInput) shouldBe 8018
   }
   it should "part2Test" in {
-    day.part2(day.getTestInput) shouldBe -1
+    day.part2(day.getTestInput) shouldBe 70
   }
   it should "part2" in {
-    day.part2(day.getInput) shouldBe -1
+    day.part2(day.getInput) shouldBe 2518
   }
 }
