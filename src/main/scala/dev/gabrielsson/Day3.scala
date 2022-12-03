f15bd7643c9cbf2b61a41d860fa204c4ed016ae4
Max Gabrielsson <max.gabrielsson@gmail.com>
2022-12-03 12:09:47 +0100

    Intersect! FML
    Signed-off-by: Max Gabrielsson <max.gabrielsson@gmail.com>

--- src/main/scala/dev/gabrielsson/Day3.scala
+++ src/main/scala/dev/gabrielsson/Day3.scala
@@ -3,17 +3,17 @@ package dev.gabrielsson
 class Day3 extends Inputs {
 
   def part1(input: Seq[String]): Int = {
-    input.map(s => (s.substring(0, s.length/2), s.substring(s.length/2)))
-      .map(t => t._1.filter(c => t._2.contains(c)))
-      .map(_.toCharArray.head)
+    input.map(s => s.splitAt(s.length / 2))
+      .map(t => t._1 intersect t._2)
+      .map(_.head)
       .map(1 + "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".indexOf(_))
       .sum
   }
 
   def part2(input: Seq[String]): Int = {
     input.grouped(3)
-      .map(g => g.head.filter(s => g(1).contains(s)).filter(s => g(2).contains(s)))
-      .map(_.toCharArray.head)
+      .map(g => g.head intersect g(1) intersect g(2))
+      .map(_.head)
       .map(1 + "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".indexOf(_))
       .sum
   }
