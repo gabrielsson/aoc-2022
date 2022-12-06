d9a0dc8613601449843a3d019712433b4c00f729
Max Gabrielsson <max.gabrielsson@gmail.com>
2022-12-06 07:02:23 +0100

    Distinct
    Signed-off-by: Max Gabrielsson <max.gabrielsson@gmail.com>

--- src/main/scala/dev/gabrielsson/Day6.scala
+++ src/main/scala/dev/gabrielsson/Day6.scala
@@ -2,13 +2,13 @@ package dev.gabrielsson
 
 class Day6 extends Inputs {
 
-  def part1(input: Seq[String]): Int = puzzle(input, 4)
+  def part1(input: Seq[String]): Int = process(input, 4)
+  
+  def part2(input: Seq[String]): Int = process(input, 14)
 
-
-  def part2(input: Seq[String]): Int = puzzle(input, 14)
-
-  private def puzzle(input: Seq[String], charUnique: Int) =
-    input.head.sliding(charUnique)
-    .takeWhile(_.toSet.size != charUnique).size + charUnique
+  private def process(input: Seq[String], noOfChars: Int) =
+    input.head.sliding(noOfChars)
+      .takeWhile(_.distinct.length != noOfChars)
+      .size + noOfChars
 
 }
