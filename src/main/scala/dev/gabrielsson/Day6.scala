ff6537075bc1265adc14dab8b85963ee3618a09d
Max Gabrielsson <max.gabrielsson@gmail.com>
2022-12-06 06:57:02 +0100

    Rename
    Signed-off-by: Max Gabrielsson <max.gabrielsson@gmail.com>

--- src/main/scala/dev/gabrielsson/Day6.scala
+++ src/main/scala/dev/gabrielsson/Day6.scala
@@ -2,12 +2,12 @@ package dev.gabrielsson
 
 class Day6 extends Inputs {
 
-  def part1(input: Seq[String]): Int = puzzle2(input, 4)
+  def part1(input: Seq[String]): Int = puzzle(input, 4)
 
 
-  def part2(input: Seq[String]): Int = puzzle2(input, 14)
+  def part2(input: Seq[String]): Int = puzzle(input, 14)
 
-  private def puzzle2(input: Seq[String], charUnique: Int) =
+  private def puzzle(input: Seq[String], charUnique: Int) =
     input.head.sliding(charUnique)
     .takeWhile(_.toSet.size != charUnique).size + charUnique
 
