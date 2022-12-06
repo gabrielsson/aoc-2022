5d211ede35a6acbc023b9695a625132a92e4a9df
Max Gabrielsson <max.gabrielsson@gmail.com>
2022-12-06 06:56:04 +0100

    Take while
    Signed-off-by: Max Gabrielsson <max.gabrielsson@gmail.com>

--- src/main/scala/dev/gabrielsson/Day6.scala
+++ src/main/scala/dev/gabrielsson/Day6.scala
@@ -2,16 +2,13 @@ package dev.gabrielsson
 
 class Day6 extends Inputs {
 
-  def part1(input: Seq[String]): Int = puzzle(input, 4)
+  def part1(input: Seq[String]): Int = puzzle2(input, 4)
 
 
-  def part2(input: Seq[String]): Int = puzzle(input, 14)
+  def part2(input: Seq[String]): Int = puzzle2(input, 14)
 
-  private def puzzle(input: Seq[String], charUnique: Int) = input.head.sliding(charUnique)
-    .foldLeft((0, false))((b, a) => b match {
-      case t if t._2 => t
-      case t if a.toSet.size == charUnique => (t._1 + charUnique, true)
-      case t => (t._1 + 1, false)
-    })._1
+  private def puzzle2(input: Seq[String], charUnique: Int) =
+    input.head.sliding(charUnique)
+    .takeWhile(_.toSet.size != charUnique).size + charUnique
 
 }
