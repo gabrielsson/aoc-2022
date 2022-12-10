5580bbc43e9a66c8d628806a35d3389606af97e2
Max Gabrielsson <max.gabrielsson@gmail.com>
2022-12-10 13:34:10 +0100

    Monads everywhere
    Signed-off-by: Max Gabrielsson <max.gabrielsson@gmail.com>

--- src/main/scala/dev/gabrielsson/Day10.scala
+++ src/main/scala/dev/gabrielsson/Day10.scala
@@ -5,45 +5,48 @@ class Day10 extends Inputs {
   private val Noop = """noop""".r
 
   def part1(input: Seq[String]): Long = {
-    val res = input.foldLeft((List[State](State(0, 1)), State(0, 1)))((s, instruction) => {
-      val states = s._1
-      val leftState = s._2
-      instruction match {
-        case AddX(v) => (states :+ leftState.inc :+ leftState.inc.inc, leftState.inc.inc.addx(v.toInt))
-        case Noop() => (states :+ leftState.inc, leftState.inc)
+    input.foldLeft(Some(List[State](State(0, 1)), State(0, 1))) {
+      case (Some((states, leftState)), instruction) =>
+        instruction match {
+          case AddX(v) => Some((states :+ leftState.inc :+ leftState.inc.inc, leftState.inc.inc.addx(v.toInt)))
+          case Noop() => Some((states :+ leftState.inc, leftState.inc))
+        }
+    }
+      .map {
+        case (states: List[State], leftState: State) => (states :+ leftState).map(s => (s.clock -> s))
+        case _ => Seq[(Long, State)]()
       }
-    })
-    val stateMap = (res._1 :+ res._2.copy(clock = res._2.clock + 1))
-      .map(s => (s.clock, s))
-      .toMap
-
-    Seq.range(20, stateMap.size, 40)
-      .map(stateMap(_))
-      .map(_.signal)
-      .sum
+      .map(l => l.toMap)
+      .map(stateMap => {
+        Seq.range(20, stateMap.size, 40)
+          .map(stateMap(_))
+          .map(_.signal)
+          .sum
+      }).getOrElse(-1L)
   }
 
   def part2(input: Seq[String]): Long = {
-    val res = input.foldLeft((List[State](State(0, 1)), State(0, 1)))((s, instruction) => {
-      val states = s._1
-      val leftState = s._2
-      instruction match {
-        case AddX(v) => (states :+ leftState.inc :+ leftState.inc.inc, leftState.inc.inc.addx(v.toInt))
-        case Noop() => (states :+ leftState.inc, leftState.inc)
-      }
-    })
-    (res._1 :+ res._2.copy(clock = res._2.clock + 1))
-      .map(s => (s.clock, s.x)).sortBy(_._1)
-      .tail.grouped(40).foreach(l => {
-      l.foreach(t => {
-        if (t._2 - 1 to t._2 + 1 contains t._1 % 40 - 1) {
-          print("#")
-        } else {
-          print(".")
+    input.foldLeft(Some(List[State](State(0, 1)), State(0, 1))) {
+      case (Some((states, leftState)), instruction) =>
+        instruction match {
+          case AddX(v) => Some((states :+ leftState.inc :+ leftState.inc.inc, leftState.inc.inc.addx(v.toInt)))
+          case Noop() => Some((states :+ leftState.inc, leftState.inc))
         }
-      })
-      println()
-    })
+    }
+      .map {
+        case (states: List[State], leftState: State) => (states :+ leftState.inc).map(s => (s.clock -> s.x))
+      }
+      .foreach(l => l.tail.grouped(40).foreach(l => {
+        l.foreach(t => {
+          if (t._2 - 1 to t._2 + 1 contains t._1 % 40 - 1) {
+            print("#")
+          } else {
+            print(".")
+          }
+        })
+        println()
+      }))
+
     -1
   }
 
